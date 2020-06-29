7fd1e510eb98af68b14e7321c0af3f78ad842464
Pepszo <memodebouvy@hotmail.com>
2020-06-30 00:52:30 +0200

    Debug errors (json infinite recursion, changing database at start) Adding first getmapping

--- src/main/java/com/school/beans/Role.java
+++ src/main/java/com/school/beans/Role.java
@@ -1,4 +1,50 @@
 package com.school.beans;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+/*@JsonIdentityInfo(
+        generator = ObjectIdGenerator.PropertyGenerator.class,
+        property = "idRole"
+)*/
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idRole", nullable = false)
+    private int idRole;
+
+    @Column(name = "label")
+    private String label;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    public Set<User> users;
+
+    public int getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
--- src/main/java/com/school/beans/User.java
+++ src/main/java/com/school/beans/User.java
@@ -1,4 +1,83 @@
 package com.school.beans;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+/*@JsonIdentityInfo(
+        generator = ObjectIdGenerator.PropertyGenerator.class,
+        property = "idUser"
+)*/
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUser", nullable = false)
+    private int idUser;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "pass")
+    private String pass;
+
+    @JsonBackReference
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    @JoinColumn(name = "idRole")
+    private Role role;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
